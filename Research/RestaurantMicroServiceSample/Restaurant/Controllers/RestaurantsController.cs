843afa1a1db9fec56fb6b95ff8b62b1989235129
Steven <steven.bouche@outlook.com>
2020-10-10 16:21:56 +0200

    Add micro service projet API sample

---
+++ Research/RestaurantMicroServiceSample/Restaurant/Controllers/RestaurantsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restaurant.Model;
+using Restaurant.Services;
+using System.Collections.Generic;
+
+namespace Restaurant.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class RestaurantsController : ControllerBase
+    {
+
+        private readonly IRestaurantManager Manager;
+
+        public RestaurantsController(IRestaurantManager manager)
+        {
+            this.Manager = manager;
+        }
+
+        // GET: api/<RestaurantController>
+        [HttpGet]
+        public ActionResult<IEnumerable<RestaurantModel>> Get([FromQuery] SearchRestaurants search)
+        {
+
+            if (search is null) search = new SearchRestaurants { page = 1, pagesize = 10, name = "" };
+
+            return Ok(this.Manager.Get(search));
+        }
+
+        // GET api/<RestaurantController>/5
+        [HttpGet("{id}")]
+        public RestaurantModel Get(string id)
+        {
+            return this.Manager.Get(id);
+        }
+
+        // POST api/<RestaurantController>
+        [HttpPost]
+        public void Post([FromBody] RestaurantModel value)
+        {
+            this.Manager.Create(value);
+        }
+
+        // PUT api/<RestaurantController>/5
+        [HttpPut("{id}")]
+        public void Put(string id, [FromBody] RestaurantModel value)
+        {
+            this.Manager.Update(id, value);
+        }
+
+        // DELETE api/<RestaurantController>/5
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            this.Manager.Remove(id);
+        }
+
+
+
+    }
+}
---
+++ Research/RestaurantMicroServiceSample/Restaurant/StartupConfig/RestaurantMongoDBConfig.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Restaurant.DataAccess;
+using Microsoft.Extensions.Options;
+using System;
+using MongoDBAccess;
+using Restaurant.Model;
+
+namespace Restaurant.StartupConfig
+{
+    public class RestaurantMongoDBConfig
+    {
+
+        public static void ConfigureService(IServiceCollection services, Func<string,IConfigurationSection> config)
+        {
+            //Load mongodb settings in RestaurantDatabaseSetting where is in appsettings.json with name : RestaurantDatabaseSetting
+            services.Configure<RestaurantDatabaseSetting>(config(nameof(RestaurantDatabaseSetting)));
+            //Define an interface who serve setting mongodb access
+            services.AddSingleton<IRestaurantsDatabaseSetting>(sp => sp.GetRequiredService<IOptions<RestaurantDatabaseSetting>>().Value);
+            //Define Restaurant service data access 
+            services.AddTransient<IMongoDBContext<RestaurantModel>, MongoDBContext<RestaurantModel, IRestaurantsDatabaseSetting>>();
+        }
+
+    }
+
+}
