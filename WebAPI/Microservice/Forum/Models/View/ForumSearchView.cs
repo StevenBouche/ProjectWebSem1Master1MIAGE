1673cb85d4f9cbe48fb9b5ccb8988eda9293fcd0
Steven <steven.bouche@outlook.com>
2020-11-07 14:44:36 +0100

    Add forum view object API

---
+++ WebAPI/Microservice/Forum/Models/View/ForumSearchView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Models.View
+{
+    public class ForumSearchView
+    {
+
+        public int TotalItem { get; set; }
+        public int TotalPage { get; set; }
+        public int CurrentPage { get; set; }
+        public int NbItemPerPage { get; set; }
+        public string NameFilter { get; set; }
+        public string DescFilter { get; set; }
+        public List<ForumView> ForumSearch { get; set; }
+
+
+
+    }
+}
---
+++ WebAPI/Microservice/Forum/Models/View/ForumView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Forum.Models.View
+{
+    public class ForumView
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+        [JsonPropertyName("nbOnline")]
+        public int NbOnline { get; set; }
+        [JsonPropertyName("nbMember")]
+        public int NbMember { get; set; }
+
+
+    }
+}
