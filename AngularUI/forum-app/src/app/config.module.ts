63d90a205d1c094c7ed507408c1f9d1a3ec66df6
haawyn <armand-prevot@hotmail.fr>
2020-11-10 12:50:27 +0100

    Auto stash before merge of "main" and "origin/main"

--- AngularUI/forum-app/src/app/config.module.ts
+++ AngularUI/forum-app/src/app/config.module.ts
@@ -3,10 +3,10 @@ export class Config {
 
   public static readonly isDev = true;
 
-  public  static readonly API_URL : string = Config.getURLAPI() +":7000";
+  public static readonly API_URL : string = Config.getURLAPI() +":7000";
   public static API_WS: string = Config.getURLAPI() + ":8081";
 
-  private static getURLAPI(): string {
+  static getURLAPI(): string {
     if(Config.isDev) return "http://localhost";
     else return "http://51.210.181.145";
   }
